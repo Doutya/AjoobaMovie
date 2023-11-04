355b31ca3a3ec6196069fdfa5bf8f8640f4cfeeb
gaya1209 <gayathri.reddy@doutya.com>
2023-11-04 22:28:04 +0530

    Added all files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'cream': '#FDF5E6', // Define your custom cream color here
+      },
+    },
+  },
+  plugins: [],
+}
