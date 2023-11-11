b19513124161cac15444344cff75abb37712198e
gaya1209 <gayathri.reddy@doutya.com>
2023-11-11 12:54:13 +0530

    added files

---
+++ Backend/firebase/firebasecustom.js
@@ -0,0 +1,19 @@
+// const { auth, firestore, messaging, usersCollectionRef } = require('../../src/firebase.config');
+
+// const customAuth = {
+//   signInWithRedirect: (provider) => {
+//     // Implement custom sign-in logic here
+//   },
+//   getRedirectResult: () => {
+//     // Implement custom redirect result handling here
+//   },
+// };
+
+// const firebaseConfig = {
+//   auth: customAuth,
+//   firestore,
+//   messaging,
+//   usersCollectionRef,
+// };
+
+// module.exports = firebaseConfig;
